97afcf34353d9c4c89b221d8c785fb23a80a0577
Danila Saikin <saykin_777@mail.ru>
2024-05-20 20:35:16 +0300

    Практическое задание maven

---
+++ src/main/java/org/example/GeometryApp.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class GeometryApp {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Circle Perimeter: " + circle.calculatePerimeter());
+
+        Rectangle rectangle = new Rectangle(4, 6);
+        System.out.println("Rectangle Area: " + rectangle.calculateArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.calculatePerimeter());
+
+        Triangle triangle = new Triangle(3, 4, 5);
+        System.out.println("Triangle Area: " + triangle.calculateArea());
+        System.out.println("Triangle Perimeter: " + triangle.calculatePerimeter());
+    }
+}
